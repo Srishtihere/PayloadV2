211e6056b98cd62bd547ef4c99adcd9688530da5
Srishti Majumder <srishtimajumder2002@gmail.com>
2024-01-10 01:55:00 +0530

    sidebar dropdown updated

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,13 @@
-import { Nav, NavItem, NavLink, NavbarBrand } from "reactstrap";
+import {
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarBrand,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu,
+  UncontrolledDropdown,
+} from "reactstrap";
 import style from "./Navbar.module.css";
 import navbarList from "../../utils/navbarList";
 import logo from "../../assets/Navlogo.jpg";
@@ -17,7 +26,18 @@ const Navbar: React.FC = () => {
               </div>
             </div>
             <div className="d-flex ps-3 justify-content-center align-items-center w-25">
-              <svg
+              <UncontrolledDropdown>
+                <DropdownToggle
+                  caret
+                  className={style.dropNav}
+                ></DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>Log IN</DropdownItem>
+                  <DropdownItem>Sign UP</DropdownItem>
+                  <DropdownItem>My Account</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              {/* <svg
                 width="16"
                 height="10"
                 viewBox="0 0 16 10"
@@ -28,7 +48,7 @@ const Navbar: React.FC = () => {
                   d="M0.846847 1.22185C1.26727 0.801426 1.92516 0.763206 2.38876 1.10719L2.52157 1.22185L8 6.69974L13.4784 1.22185C13.8988 0.801426 14.5567 0.763206 15.0203 1.10719L15.1532 1.22185C15.5736 1.64227 15.6118 2.30016 15.2678 2.76376L15.1532 2.89657L8.83736 9.21236C8.41694 9.63278 7.75905 9.671 7.29545 9.32702L7.16264 9.21236L0.846847 2.89657C0.384384 2.43411 0.384384 1.68431 0.846847 1.22185Z"
                   fill="white"
                 />
-              </svg>
+              </svg> */}
             </div>
           </NavbarBrand>
         </div>
